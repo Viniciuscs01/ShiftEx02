e4d57bd2112140f87a9b79c1d2a6631fd5956258
Vinícius <vinicius.cs01>
2017-07-15 17:17:19 -0300

    Add project files.

---
+++ ShiftEx/ShiftEx/Config.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShiftEx
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Config : ContentPage
+    {
+        public bool Enviar { get { return enviar.On; } }
+        public string Email { get { return txtNome.Text; } }
+        public Config()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ ShiftEx/ShiftEx/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShiftEx
+{
+    public partial class MainPage : ContentPage
+    {
+        Config configPage;
+        public MainPage()
+        {
+            configPage = new Config();
+            InitializeComponent();
+            BindingContext = configPage;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(configPage);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (configPage.Enviar)
+                DisplayAlert("Teste", $"Email enviado para {configPage.Email}", "Ok");
+            else
+                DisplayAlert("Teste", "Email não configurado", "Ok");
+        }
+    }
+}
